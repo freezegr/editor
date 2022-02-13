bf472049e0dbdf62405a31b14f9f93aa60dbacf9
freezegr <Statsaras2006@gmail.com>
2022-02-13 21:08:26 +0200

    v0.0.1

--- website/assets/js/socket.js
+++ website/assets/js/socket.js
@@ -45,3 +45,5 @@ function addSubtitles(opts){
 
 //<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 //<script src="assets/js/socket.js"></script>
+//<script src="highlight-ta.js"></script>
+//<script src="highlight-ta-proto.js"></script>
