896f88c923c6015c0c7d8c5b06bdee0b8144e063
freezegr <Statsaras2006@gmail.com>
2022-02-15 22:50:39 +0200

    v0.0.1

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const { app, BrowserWindow, Menu, dialog } = require('electron');
-const menus =  require('./src/menu.js');
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron');
+const menus = require('./src/menu.js');
+const path = require('path');
+const { exportsTitles } = require('./src/exportsTitles.js');
+const fs = require("fs");
 
-function createWindow () {
+function createWindow() {
   let mainWindow = new BrowserWindow({
     width: 1100,
     height: 800,
@@ -22,6 +25,29 @@ app.whenReady().then(() => {
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 })
+
+ipcMain.on("exportsAss", (event, data) => {
+  console.log(data)
+  dialog.showSaveDialog({
+    title: 'Select the File Path to save',
+    buttonLabel: 'Save',
+    defaultPath: path.join(__dirname, '/subtitles.ass'),
+    filters: [
+      {
+        name: 'ass/ssa',
+        extensions: ['ass']
+      },],
+    properties: []
+  }).then(file => {
+    if (!file.canceled) {
+      fs.writeFile(file.filePath.toString(), exportsTitles(data), function (err) {
+        if (err) throw err;
+      });
+    }
+  }).catch(err => {
+    console.log(err)
+  });
+});
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
--- website/assets/js/socket.js
+++ website/assets/js/socket.js
@@ -1,6 +1,7 @@
-const { ipcRenderer, remote } = require('electron');
+const { ipcRenderer, remote, ipcMain } = require('electron');
 
 let titles; 
+let datas;
 
 ipcRenderer.on('subtitles', (event, data) => {
     document.title = data.filename;
@@ -8,6 +9,7 @@ ipcRenderer.on('subtitles', (event, data) => {
     const elem = document.getElementsByClassName('subtitlesBoxFormsAll');
     $(".subtitlesBoxFormsAll").empty()
     titles = subttitles
+    datas = data.subtitles
     subttitles.forEach((x, count) => {
         x.value.count = count
         let txt = addSubtitles(x.value)
@@ -19,8 +21,8 @@ ipcRenderer.on('videoOpen', (event, data) => {
   $(".video")[0].src = data;
 });
 
-ipcRenderer.on('saveFileAs', (event, data) => {
-  exportsTitles()
+ipcRenderer.on('saveFileAs', () => {
+  ipcRenderer.send('exportsAss', datas)
 });
 
 //8umisou stathi na baleis kapoia stigmi otan einai to idio row na min kanei tpt
@@ -55,31 +57,6 @@ function addSubtitles(opts){
       </tr>
     `
     return text;
-}
-
-const exportsTitles = (data) => {
-  let str = '';
-  data.forEach(x => {
-      str += `[${x.section}]\n`
-      x.body.forEach((l, count) => {
-          if(l.type == "comment"){
-              str += "; " + l.value + "\n"
-          } else if(l.key){
-              if(typeof l.value == "string"){
-                  str += `${l.key}: ${l.value}\n`
-              } else if(typeof l.value == "object"){
-                  let val = Object.values(l.value).join()
-                  str += `${l.key}: ${val}\n`
-              }
-          }
-          if(x.body.length == count+1) str += '\n'
-      })
-  });
-
-  const {dialog} = require('electron');
-  let fs = require('fs');
-  fs.writeFileSync()
-  return str
 }
 
 //<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
